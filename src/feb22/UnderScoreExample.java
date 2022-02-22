923a77954ce32584d32fb6baff54b3ad4f05ba4a
ankushbharambe902 <99001885+ankushbharambe902@users.noreply.github.com>
2022-02-22 22:50:12 +0530

    Update UnderScoreExample.java

--- src/feb22/UnderScoreExample.java
+++ src/feb22/UnderScoreExample.java
@@ -8,10 +8,10 @@ public class UnderScoreExample {
 }
 /*
 * Output:
-* error.   _ it canot be used as identifier
+* error.   _ it can not be used as identifier
 
-Process finished with exit code 0
-Reason because the java support the special character $ and _ for the variable name
+
+
 *
 *
 * */
