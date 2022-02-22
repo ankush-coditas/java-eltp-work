f2432579ed5842c5f25e7e8942cc1925bfd63ac1
ankushbharambe902 <99001885+ankushbharambe902@users.noreply.github.com>
2022-02-22 22:49:20 +0530

    Update UnderScoreExample.java

--- src/feb22/UnderScoreExample.java
+++ src/feb22/UnderScoreExample.java
@@ -8,7 +8,7 @@ public class UnderScoreExample {
 }
 /*
 * Output:
-* 50
+* error.   _ it canot be used as identifier
 
 Process finished with exit code 0
 Reason because the java support the special character $ and _ for the variable name
